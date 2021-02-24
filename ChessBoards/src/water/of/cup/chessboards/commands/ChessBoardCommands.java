7df9f6e4b746d7935573a799fdc58a48405b228c
Achoowy <Achoowy@gmail.com>
2021-02-24 11:29:50 -0600

    Added '/chessboards stats [player name]' command

--- ChessBoards/src/water/of/cup/chessboards/commands/ChessBoardCommands.java
+++ ChessBoards/src/water/of/cup/chessboards/commands/ChessBoardCommands.java
@@ -1,5 +1,6 @@
 package water.of.cup.chessboards.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -12,6 +13,8 @@ import water.of.cup.chessboards.chessBoard.ChessUtils;
 import water.of.cup.chessboards.data.ChessPlayer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class ChessBoardCommands implements CommandExecutor {
@@ -32,8 +35,8 @@ public class ChessBoardCommands implements CommandExecutor {
 			if (permissionsEnabled && !p.hasPermission("chessboard.command"))
 				return false;
 
-			if(args.length > 0) {
-				if(args[0].equalsIgnoreCase("give")) {
+			if (args.length > 0) {
+				if (args[0].equalsIgnoreCase("give")) {
 					if (permissionsEnabled && !p.hasPermission("chessboard.command.give"))
 						return false;
 
@@ -44,11 +47,11 @@ public class ChessBoardCommands implements CommandExecutor {
 					return chessBoardGiven;
 				}
 
-				if(args[0].equalsIgnoreCase("leaderboard")) {
+				if (args[0].equalsIgnoreCase("leaderboard")) {
 					if (permissionsEnabled && !p.hasPermission("chessboard.command.leaderboard"))
 						return false;
 
-					if(!databaseEnabled) {
+					if (!databaseEnabled) {
 						p.sendMessage("Database configuration must be on in order to view leaderboard");
 						return false;
 					}
@@ -56,45 +59,93 @@ public class ChessBoardCommands implements CommandExecutor {
 					int numChessPlayers = instance.getDataStore().getChessPlayerTotal();
 
 					int page = 0;
-					if(args.length > 1) {
+					if (args.length > 1) {
 						try {
 							page = Integer.parseInt(args[1]) - 1;
-						} catch (NumberFormatException e) {}
+						} catch (NumberFormatException e) {
+						}
 					}
 
-					if(page < 0) page = 0;
+					if (page < 0)
+						page = 0;
 
 					int numOfPages = (numChessPlayers / 10) + 1;
 
-					if(page > numOfPages - 1) page = numOfPages - 1;
+					if (page > numOfPages - 1)
+						page = numOfPages - 1;
 
 					ArrayList<ChessPlayer> topPlayers = instance.getDataStore().getTopPlayers(page);
 
-					if(topPlayers == null) {
+					if (topPlayers == null) {
 						p.sendMessage("There was an error while trying to fetch top players");
 						return false;
 					}
 
-					p.sendMessage(ChatColor.WHITE + "" + ChatColor.BOLD + "Chess" + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Boards " + ChatColor.RESET + "Leaderboard (" + (page + 1) + "/" + numOfPages + ")");
+					p.sendMessage(ChatColor.WHITE + "" + ChatColor.BOLD + "Chess" + ChatColor.DARK_GRAY + ""
+							+ ChatColor.BOLD + "Boards " + ChatColor.RESET + "Leaderboard (" + (page + 1) + "/"
+							+ numOfPages + ")");
 					int num = 1 + (page * 10);
-					for(ChessPlayer chessPlayer : topPlayers) {
+					for (ChessPlayer chessPlayer : topPlayers) {
 						UUID chessPlayerUUID = UUID.fromString(chessPlayer.getUuid());
 						OfflinePlayer player = instance.getServer().getOfflinePlayer(chessPlayerUUID);
-						if(player == null) continue;
+						if (player == null)
+							continue;
 
 						double ratingRounded = (double) Math.round(chessPlayer.getRating() * 100) / 100;
-						p.sendMessage(ChatColor.GRAY + "" + num + ". " + ChatColor.RESET + "" + player.getName() + " - " + ratingRounded);
+						p.sendMessage(ChatColor.GRAY + "" + num + ". " + ChatColor.RESET + "" + player.getName() + " - "
+								+ ratingRounded);
 						num++;
 					}
 					return true;
 				}
+
+				if (args[0].equalsIgnoreCase("stats")) {
+					if (permissionsEnabled && !p.hasPermission("chessboard.command.stats"))
+						return false;
+
+					if (!databaseEnabled) {
+						p.sendMessage("Database configuration must be on in order to view player stats");
+						return false;
+					}
+
+					if (args.length > 1) {
+						// next arg is playername
+						String name = args[1];
+
+						for (ChessPlayer chessPlayer : instance.getDataStore().getChessPlayers().values()) {
+							UUID chessPlayerUUID = UUID.fromString(chessPlayer.getUuid());
+							OfflinePlayer player = instance.getServer().getOfflinePlayer(chessPlayerUUID);
+							if (player == null)
+								continue;
+
+							if (player.getName().equals(name)) {
+								// player found, send stats message
+								p.sendMessage(ChatColor.DARK_BLUE + "--" + ChatColor.BLUE + name + "'s chess stats" + ChatColor.DARK_BLUE + "--");
+								p.sendMessage(ChatColor.BLUE + "Rating: " + (double) Math.round(chessPlayer.getRating() * 100) / 100);
+								//p.sendMessage(ChatColor.BLUE + "Games: " + chessPlayer.getNumberOfResults());
+								p.sendMessage(ChatColor.BLUE + "W/L/D: " + chessPlayer.getWins() + " W / " + chessPlayer.getLosses() + " L / " + chessPlayer.getTies() + " D");
+								return true;
+							}
+						}
+
+						p.sendMessage("player not found in chess database");
+						return false;
+					}
+					// missing arg
+
+					// TODO: Change message
+					p.sendMessage("/chessboards stats [player name]" + ChatColor.GRAY + ": Show stats for a player");
+					return false;
+				}
 			}
 			// Send help message
-			p.sendMessage(ChatColor.WHITE + "" + ChatColor.BOLD + "Chess" + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "Boards");
+			p.sendMessage(ChatColor.WHITE + "" + ChatColor.BOLD + "Chess" + ChatColor.DARK_GRAY + "" + ChatColor.BOLD
+					+ "Boards");
 			p.sendMessage("/chessboards give" + ChatColor.GRAY + ": Gives you chessboard");
 			p.sendMessage("/chessboards leaderboard" + ChatColor.GRAY + ": Lists top chess players");
+			p.sendMessage("/chessboards stats [player name]" + ChatColor.GRAY + ": Show stats for a player");
 			return false;
-			
+
 		}
 		return false;
 	}
--- ChessBoards/src/water/of/cup/chessboards/commands/ChessBoardCommandsTabCompleter.java
+++ ChessBoards/src/water/of/cup/chessboards/commands/ChessBoardCommandsTabCompleter.java
@@ -1,8 +1,11 @@
 package water.of.cup.chessboards.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
 import water.of.cup.chessboards.ChessBoards;
 
 import java.util.ArrayList;
@@ -10,22 +13,30 @@ import java.util.List;
 
 public class ChessBoardCommandsTabCompleter implements TabCompleter {
 
-    private ChessBoards instance = ChessBoards.getInstance();
+	private ChessBoards instance = ChessBoards.getInstance();
 
-    @Override
-    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
-        ArrayList<String> arguments = new ArrayList<>();
-        if(strings.length == 1) {
-            arguments.add("leaderboard");
-            arguments.add("give");
-        } else if(strings.length == 2 && strings[0].equalsIgnoreCase("leaderboard")) {
-            int numChessPlayers = instance.getDataStore().getChessPlayerTotal();
-            int extraPages = (numChessPlayers / 10);
-            for(int i = 0; i < extraPages; i++) {
-                arguments.add((i + 2) + "");
-            }
-        }
+	@Override
+	public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+		ArrayList<String> arguments = new ArrayList<>();
+		if (strings.length == 1) {
+			arguments.add("leaderboard");
+			arguments.add("give");
+			arguments.add("stats");
+		} else if (strings.length == 2) {
+			if (strings[0].equalsIgnoreCase("leaderboard")) {
+				int numChessPlayers = instance.getDataStore().getChessPlayerTotal();
+				int extraPages = (numChessPlayers / 10);
+				for (int i = 0; i < extraPages; i++) {
+					arguments.add((i + 2) + "");
+				}
+			} else if (strings[0].equalsIgnoreCase("stats")) {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					arguments.add(player.getName());
+				}
+				
+			}
+		}
 
-        return arguments;
-    }
+		return arguments;
+	}
 }
